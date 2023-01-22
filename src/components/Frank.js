cb881ffcf0c435971b46fdfd32d8b7fc78452133
esul-gif <122951430+esul-gif@users.noreply.github.com>
2023-01-21 19:05:53 -0500

    Update Frank.js

--- src/components/Frank.js
+++ src/components/Frank.js
@@ -2,6 +2,7 @@ function Frank() {
     return (
         <div style={{height: '300px', textAlign:'center', backgroundColor: 'grey'}}>
             <h1> Frank Page </h1>
+            <p> Love you frank </p>
         </div>
     )
 }
